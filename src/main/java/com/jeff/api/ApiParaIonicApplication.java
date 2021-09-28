d29d04d53e3e356895b88d8c749989f0bb6a1052
JEFERSON FILIPE  SERGIO <jeferson.filipe@inmetrics.com.br>
2021-09-28 14:16:15 -0300

    first commit

---
+++ src/main/java/com/jeff/api/ApiParaIonicApplication.java
@@ -0,0 +1,13 @@
+package com.jeff.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiParaIonicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiParaIonicApplication.class, args);
+	}
+
+}
