a4d61e9576c9473b04a66c41d0fe8988ae793940
JEFERSON FILIPE  SERGIO <jeferson.filipe@inmetrics.com.br>
2021-10-01 11:56:52 -0300

    Entidades Cidade e Estado

--- src/main/java/com/jeff/api/config/TestConfig.java
+++ src/main/java/com/jeff/api/config/TestConfig.java
@@ -8,8 +8,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.jeff.api.domain.Categoria;
+import com.jeff.api.domain.Cidade;
+import com.jeff.api.domain.Estado;
 import com.jeff.api.domain.Produto;
 import com.jeff.api.repositories.CategoriaRepository;
+import com.jeff.api.repositories.CidadeRepository;
+import com.jeff.api.repositories.EstadoRepository;
 import com.jeff.api.repositories.ProdutoRepository;
 
 @Configuration
@@ -21,6 +25,12 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
@@ -39,8 +49,21 @@ public class TestConfig implements CommandLineRunner{
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade cid1 = new Cidade(null, "Ubêrlandia", est1);
+		Cidade cid2 = new Cidade(null, "São Paulo", est2);
+		Cidade cid3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(cid1));
+		est2.getCidades().addAll(Arrays.asList(cid2, cid3));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(cid1, cid2, cid3));
+		
 	}
 
 }
---
+++ src/main/java/com/jeff/api/repositories/EstadoRepository.java
@@ -0,0 +1,9 @@
+package com.jeff.api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jeff.api.domain.Estado;
+
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
