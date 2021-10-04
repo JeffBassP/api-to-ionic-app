97aea47a8b59b5c4c2ab393ff1798c91dcd2f67f
JEFERSON FILIPE  SERGIO <jeferson.filipe@inmetrics.com.br>
2021-10-04 14:09:33 -0300

    Inserindo categoria com metodo POST

--- src/main/java/com/jeff/api/resources/CategoriaResource.java
+++ src/main/java/com/jeff/api/resources/CategoriaResource.java
@@ -1,13 +1,17 @@
 package com.jeff.api.resources;
 
+import java.net.URI;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.jeff.api.domain.Categoria;
 import com.jeff.api.service.CategoriaService;
@@ -23,6 +27,13 @@ public class CategoriaResource {
 		Optional<Categoria> obj = service.findOne(id);
 		
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
  }
