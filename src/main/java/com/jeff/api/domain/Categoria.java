ca5a7276fc71d2f36e666c8480282a0ffc837ae9
JEFERSON FILIPE  SERGIO <jeferson.filipe@inmetrics.com.br>
2021-10-07 14:28:10 -0300

    paginacao com paramentros opcionais Page

--- src/main/java/com/jeff/api/domain/Categoria.java
+++ src/main/java/com/jeff/api/domain/Categoria.java
@@ -18,7 +18,7 @@ public class Categoria implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	private String name;
+	private String nome;
 
 	@ManyToMany(mappedBy = "categorias")
 	private List<Produto> produtos = new ArrayList<>();
@@ -27,10 +27,10 @@ public class Categoria implements Serializable {
 
 	}
 
-	public Categoria(Integer id, String name) {
+	public Categoria(Integer id, String nome) {
 		super();
 		this.id = id;
-		this.name = name;
+		this.nome = nome;
 	}
 
 	public Integer getId() {
@@ -42,11 +42,11 @@ public class Categoria implements Serializable {
 	}
 
 	public String getName() {
-		return name;
+		return nome;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setName(String nome) {
+		this.nome = nome;
 	}
 
 	public List<Produto> getProdutos() {
--- src/main/java/com/jeff/api/resources/CategoriaResource.java
+++ src/main/java/com/jeff/api/resources/CategoriaResource.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -26,42 +28,57 @@ import com.jeff.api.service.CategoriaService;
 public class CategoriaResource {
 	@Autowired
 	private CategoriaService service;
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Optional<Categoria>> find(@PathVariable Integer id){
+	public ResponseEntity<Optional<Categoria>> find(@PathVariable Integer id) {
 		Optional<Categoria> obj = service.findOne(id);
-		
+
 		return ResponseEntity.ok().body(obj);
 	}
-	
+
 	@GetMapping
-	public ResponseEntity<List<CategoriaDTO>> findAll(){
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
 		List<Categoria> list = service.findAll();
 		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
-		
+
 		return ResponseEntity.ok().body(listDto);
 	}
-	
+
 	@PostMapping
-	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
-		
+
 		return ResponseEntity.created(uri).build();
 	}
-	
+
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id){
+	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id) {
 		obj.setId(id);
 		obj = service.update(obj);
-		
+
 		return ResponseEntity.noContent().build();
 	}
-	
+
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<Void> delete(@PathVariable Integer id){
 		service.delete(id);
 		
 		return ResponseEntity.noContent().build();
+	}
+
+	@GetMapping(value = "/page")
+	public ResponseEntity<Page<CategoriaDTO>> findPage(
+			@RequestParam(value = "page", defaultValue = "0") Integer page, 
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage, 
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy, 
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction){
+		
+		Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction);
+		
+		Page<CategoriaDTO> listDto = list.map(obj -> new CategoriaDTO(obj));
+		
+		return ResponseEntity.ok().body(listDto);
+		
 	}
  }
