556beb2e8f77ba6423cb1c2c7179a474ddf964eb
JEFERSON FILIPE  SERGIO <jeferson.filipe@inmetrics.com.br>
2021-10-11 18:06:13 -0300

    inserindo um novo cliente com POST

--- src/main/java/com/jeff/api/resources/ClienteResource.java
+++ src/main/java/com/jeff/api/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.jeff.api.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -12,14 +13,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.jeff.api.domain.Cliente;
 import com.jeff.api.dto.ClienteDTO;
+import com.jeff.api.dto.ClienteNewDTO;
 import com.jeff.api.service.ClienteService;
 
 @RestController
@@ -42,7 +46,7 @@ public class ClienteResource {
 
 		return ResponseEntity.ok().body(listDto);
 	}
-
+	
 
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id) {
@@ -51,6 +55,15 @@ public class ClienteResource {
 		obj = service.update(obj);
 
 		return ResponseEntity.noContent().build();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) {
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+
+		return ResponseEntity.created(uri).build();
 	}
 
 	@DeleteMapping(value = "/{id}")
--- src/main/java/com/jeff/api/service/ClienteService.java
+++ src/main/java/com/jeff/api/service/ClienteService.java
@@ -10,9 +10,15 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.jeff.api.domain.Cidade;
 import com.jeff.api.domain.Cliente;
+import com.jeff.api.domain.Endereco;
+import com.jeff.api.domain.enums.TipoCliente;
 import com.jeff.api.dto.ClienteDTO;
+import com.jeff.api.dto.ClienteNewDTO;
+import com.jeff.api.repositories.CidadeRepository;
 import com.jeff.api.repositories.ClienteRepository;
+import com.jeff.api.repositories.EnderecoRepository;
 import com.jeff.api.service.exceptions.ObjectNotFoundException;
 
 @Service
@@ -20,6 +26,12 @@ public class ClienteService {
 
 	@Autowired
 	private ClienteRepository repository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public Optional<Cliente> findOne(Integer id) {
 		Optional<Cliente> obj = repository.findById(id);
@@ -33,6 +45,14 @@ public class ClienteService {
 		updateData(newObj, obj);
 		return repository.save(newObj.get());
 	}
+	
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repository.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
+	}
+
 
 	private void updateData(Optional<Cliente> newObj, Cliente obj) {
 		newObj.get().setNome(obj.getNome());
@@ -57,6 +77,24 @@ public class ClienteService {
 	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repository.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()));
+		Optional<Cidade> cid = cidadeRepository.findById(objDto.getCidadeId());
+		
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid.get());
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		
+		if(objDto.getTelefone2() != null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if(objDto.getTelefone3() != null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		
+		return cli;
 	}
 	
 	public Cliente fromDTO(ClienteDTO objDto) {
