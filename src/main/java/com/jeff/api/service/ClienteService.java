21ed0ebabc2699571d7bb9479bb38999fcaeaefc
JEFERSON FILIPE  SERGIO <jeferson.filipe@inmetrics.com.br>
2021-10-11 12:13:29 -0300

    PUT, GET e DELETE para Cliente

--- src/main/java/com/jeff/api/service/ClienteService.java
+++ src/main/java/com/jeff/api/service/ClienteService.java
@@ -1,11 +1,17 @@
 package com.jeff.api.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.jeff.api.domain.Cliente;
+import com.jeff.api.dto.ClienteDTO;
 import com.jeff.api.repositories.ClienteRepository;
 import com.jeff.api.service.exceptions.ObjectNotFoundException;
 
@@ -21,4 +27,39 @@ public class ClienteService {
 		return Optional.of(obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo " + Cliente.class.getName())));
 	}
 
+	public Cliente update(Cliente obj) {
+		Optional<Cliente> newObj = findOne(obj.getId());
+
+		updateData(newObj, obj);
+		return repository.save(newObj.get());
+	}
+
+	private void updateData(Optional<Cliente> newObj, Cliente obj) {
+		newObj.get().setNome(obj.getNome());
+		newObj.get().setEmail(obj.getEmail());
+	}
+
+	public void delete(Integer id) {
+		findOne(id);
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityViolationException("Não é possivel excluir um cliente pois há entidades relacionadas");
+
+		}
+	}
+
+	public List<Cliente> findAll() {
+
+		return repository.findAll();
+	}
+
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repository.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
 }
