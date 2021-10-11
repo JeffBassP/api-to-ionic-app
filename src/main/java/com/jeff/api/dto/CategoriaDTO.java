4508ad154b87714a1b4a4050d40fbcd802363e8b
JEFERSON FILIPE  SERGIO <jeferson.filipe@inmetrics.com.br>
2021-10-11 10:48:53 -0300

    validacao sintatica com bean validation

--- src/main/java/com/jeff/api/dto/CategoriaDTO.java
+++ src/main/java/com/jeff/api/dto/CategoriaDTO.java
@@ -2,12 +2,20 @@ package com.jeff.api.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.jeff.api.domain.Categoria;
 
 public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
+
 	
 	private Integer id;
+	
+	@NotEmpty(message = "Preenchimento obrigatorio")
+	@Length(min = 5, max = 80, message = "O tamanho deve ser entre 5 e 80 caracteres")
 	private String name;
 	
 	public CategoriaDTO() {
