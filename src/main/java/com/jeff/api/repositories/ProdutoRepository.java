eb41ea3d3bf82130d9e652f1433ac667ca2fdc78
JEFERSON FILIPE  SERGIO <jeferson.filipe@inmetrics.com.br>
2021-10-12 17:05:08 -0300

    Adicionando o @transactional

--- src/main/java/com/jeff/api/repositories/ProdutoRepository.java
+++ src/main/java/com/jeff/api/repositories/ProdutoRepository.java
@@ -7,13 +7,16 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.jeff.api.domain.Categoria;
 import com.jeff.api.domain.Produto;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 
+	@Transactional(readOnly = true)
 	@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat in :categorias")
-	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(@Param("nome") String nome, @Param("categorias") List<Categoria> categorias, Pageable pageRequest);
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(@Param("nome") String nome,
+			@Param("categorias") List<Categoria> categorias, Pageable pageRequest);
 
 }
