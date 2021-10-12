5ecb4e2a2c1cdbdf7da1da0212016d34d0a78aaa
JEFERSON FILIPE  SERGIO <jeferson.filipe@inmetrics.com.br>
2021-10-12 14:05:51 -0300

    Validacao para nao cadastrar email repetido

--- src/main/java/com/jeff/api/repositories/ClienteRepository.java
+++ src/main/java/com/jeff/api/repositories/ClienteRepository.java
@@ -1,9 +1,12 @@
 package com.jeff.api.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.jeff.api.domain.Cliente;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
-
+	
+	@Transactional(readOnly = true)
+	Cliente findByEmail(String email);
 }
--- src/main/java/com/jeff/api/service/validation/ClientInsertValidator.java
+++ src/main/java/com/jeff/api/service/validation/ClientInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.jeff.api.domain.Cliente;
 import com.jeff.api.domain.enums.TipoCliente;
 import com.jeff.api.dto.ClienteNewDTO;
+import com.jeff.api.repositories.ClienteRepository;
 import com.jeff.api.resources.exceptions.FieldMessage;
 import com.jeff.api.service.validation.utils.BR;
 
 public class ClientInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repository;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -28,6 +36,11 @@ public class ClientInsertValidator implements ConstraintValidator<ClienteInsert,
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ Invalido"));
 		}
 
+		Cliente aux = repository.findByEmail(objDto.getEmail());
+		
+		if(aux != null) {
+			list.add(new FieldMessage("email", "Email já cadastrado!"));
+		}
 
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
