fc4fc3aa85d63a68eb4cc9c3d45e1dbed2ca7c4b
JEFERSON FILIPE  SERGIO <jeferson.filipe@inmetrics.com.br>
2021-10-12 17:04:32 -0300

    busca de pedidos por nome e categorias

--- src/main/java/com/jeff/api/resources/ProdutoResource.java
+++ src/main/java/com/jeff/api/resources/ProdutoResource.java
@@ -1,15 +1,20 @@
 package com.jeff.api.resources;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jeff.api.domain.Produto;
+import com.jeff.api.dto.ProdutoDTO;
+import com.jeff.api.resources.utils.URL;
 import com.jeff.api.service.ProdutoService;
 
 @RestController
@@ -19,10 +24,31 @@ public class ProdutoResource {
 	private ProdutoService service;
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Optional<Produto>> find(@PathVariable Long id){
+	public ResponseEntity<Optional<Produto>> find(@PathVariable Integer id){
 		Optional<Produto> obj = service.findOne(id);
 		
 		return ResponseEntity.ok().body(obj);
 	}
 	
+	@GetMapping
+	public ResponseEntity<Page<ProdutoDTO>> findPage(
+			@RequestParam(value = "nome", defaultValue = "") String nome,
+			@RequestParam(value = "categorias", defaultValue = "") String categorias,
+			@RequestParam(value = "page", defaultValue = "0") Integer page, 
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage, 
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy, 
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction){
+		
+		String nomeDecoded = URL.decodeParam(nome);
+		
+		List<Integer> ids = URL.decodeIntList(categorias);
+		
+		Page<Produto> list = service.search(nomeDecoded, ids, page, linesPerPage, orderBy, direction);
+		
+		Page<ProdutoDTO> listDto = list.map(obj -> new ProdutoDTO(obj));
+		
+		return ResponseEntity.ok().body(listDto);
+		
+	}
  }
+ 
--- src/main/java/com/jeff/api/service/ProdutoService.java
+++ src/main/java/com/jeff/api/service/ProdutoService.java
@@ -1,22 +1,44 @@
 package com.jeff.api.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.jeff.api.domain.Categoria;
 import com.jeff.api.domain.Produto;
+import com.jeff.api.repositories.CategoriaRepository;
 import com.jeff.api.repositories.ProdutoRepository;
+import com.jeff.api.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class ProdutoService {
 
 	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
 	private ProdutoRepository repository;
 
-	public Optional<Produto> findOne(Long id) {
+	public Optional<Produto> findOne(Integer id) {
 		Optional<Produto> obj = repository.findById(id);
-		return obj;
+
+		return Optional.of(obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo " + Produto.class.getName())));
 	}
 
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy,
+			String direction) {
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		List<Categoria> categorias = categoriaRepository.findAllById(ids);
+		
+		return repository.findDistinctByNomeContainingAndCategoriasIn(nome, categorias, pageRequest);
+
+	}
 }
