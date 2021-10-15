111d62e93aa775126e38b612d00949a0003a0463
JeffBassP <jeferson_jfs@hotmail.com>
2021-10-15 18:39:55 -0300

    Adicionando @Service nas classes

--- src/main/java/com/jeff/api/service/EmailService.java
+++ src/main/java/com/jeff/api/service/EmailService.java
@@ -1,9 +1,11 @@
 package com.jeff.api.service;
 
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Service;
 
 import com.jeff.api.domain.Pedido;
 
+@Service
 public interface EmailService {
 	
 	void sendOrderConfirmationEmail(Pedido obj);
