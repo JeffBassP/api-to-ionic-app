a97401ed2755e425a2f49d50bc33369576821809
JeffBassP <jeferson_jfs@hotmail.com>
2022-01-07 10:49:22 -0300

    salvando primeira imagem S3

--- src/main/java/com/jeff/api/ApiParaIonicApplication.java
+++ src/main/java/com/jeff/api/ApiParaIonicApplication.java
@@ -1,20 +1,25 @@
 package com.jeff.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.jeff.api.service.S3Service;
+
 @SpringBootApplication
 public class ApiParaIonicApplication implements CommandLineRunner {
 
+	@Autowired
+	private S3Service s3Service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ApiParaIonicApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-
+		s3Service.uploadFile("C:\\temp\\jeff.heic");
 	}
 
 }
